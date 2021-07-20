70c8406ac2af2a3c538ab07466f7f02d3f13479b
tushar <tushar.savaliya@shivlab.com>
2021-07-20 16:30:01 +0530

    update group api

--- src/constant/common.js
+++ src/constant/common.js
@@ -1,6 +1,6 @@
 let common= {
-    socket_base_url:'http://localhost:3010/',
-    common_url:'http://localhost:3010/api/v1/',
+    socket_base_url:'https://node-special-engine.herokuapp.com/',
+    common_url:'https://node-special-engine.herokuapp.com/api/v1/',
     group_join:'user/group/join',
     group_get:'group/',
 }
